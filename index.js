f0b8ff7117c35741827554a9b6ad3bc8827d0ea9
AkritiGhosh <akritighosh98@gmail.com>
2025-02-04 23:52:09 +0530

    Route + JSON

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-import dotenv  from "dotenv"
+import dotenv from "dotenv";
 import express from "express";
 import { initDB } from "./lib/dbConfig.js";
+import optionRouter from "./routes/options.js";
 
-dotenv.config()
+dotenv.config();
 
 const app = express();
+
+app.use(express.json());  // ✅ Fix: Parse JSON request bodies
+app.use(express.urlencoded({ extended: true })); // Optional for form data
 
 initDB();
 
@@ -12,6 +16,8 @@ app.get("/", (request, response) => {
   console.log(request);
   return response.status(200).send("YOLO");
 });
+
+app.use("/options", optionRouter);
 
 app.listen(process.env.PORT, () => {
   console.log("app is running ", process.env.PORT);
