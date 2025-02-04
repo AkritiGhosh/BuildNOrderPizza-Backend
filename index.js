062b0f4317c9ddcfa1fcdbd078245e06e6ecc701
rayrrren-sharbo <akriti@rapiq.io>
2025-02-04 18:36:12 +0530

    DB initialized from env variables

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-import express, { response } from "express";
-import { PORT } from "./config.js";
+import dotenv  from "dotenv"
+import express from "express";
+import { initDB } from "./lib/dbConfig.js";
+
+dotenv.config()
+
 const app = express();
+
+initDB();
+
 app.get("/", (request, response) => {
   console.log(request);
   return response.status(200).send("YOLO");
 });
-app.listen(PORT, () => {
-  console.log("app is running ", PORT);
+
+app.listen(process.env.PORT, () => {
+  console.log("app is running ", process.env.PORT);
 });
