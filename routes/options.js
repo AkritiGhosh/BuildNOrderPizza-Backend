10fed340ff6c24fc3d7e8a0bdb49dad30f209d86
AkritiGhosh <akritighosh98@gmail.com>
2025-02-05 10:42:43 +0530

    get items of a particular category - API

--- routes/options.js
+++ routes/options.js
@@ -3,6 +3,66 @@ import PizzaOptions from "../model/pizzaOptions.js";
 
 const optionRouter = express.Router();
 
+const restructurePizzaOptionJSON = (array) => {
+  return array.map((ele) => {
+    return {
+      name: ele?.name,
+      price: ele?.price,
+      isAvailable: ele?.isAvailable,
+    };
+  });
+};
+
+optionRouter.get("/", async (req, res) => {
+  try {
+    const pizzaOptions = await PizzaOptions.find();
+
+    if (!pizzaOptions || pizzaOptions.length === 0) {
+      return res.status(300).json({ msg: "No data added yet" });
+    }
+
+    // Extract unique categories
+    const categories = [...new Set(pizzaOptions.map((opt) => opt.category))];
+
+    // Categorize options
+    const categorizedOptions = categories.map((category) => ({
+      category: category,
+      data: restructurePizzaOptionJSON(
+        pizzaOptions.filter((opt) => opt.category === category)
+      ),
+    }));
+
+    return res.status(200).json({ data: categorizedOptions });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Error fetching the pizza options", error });
+  }
+});
+
+optionRouter.get("/:category", async (req, res) => {
+  try {
+    const category = req?.params?.category;
+    const pizzaOptions = await PizzaOptions.find({ category: category });
+
+    if (!pizzaOptions || pizzaOptions.length === 0) {
+      return res.status(300).json({ msg: "No data added yet" });
+    }
+
+    // Categorize options
+    const categorizedOptions = {
+      category: category,
+      data: restructurePizzaOptionJSON(pizzaOptions),
+    };
+
+    return res.status(200).json({ data: categorizedOptions });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Error fetching the pizza options", error });
+  }
+});
+
 // Add a new pizza option
 optionRouter.post("/add", async (req, res) => {
   try {
@@ -19,34 +79,9 @@ optionRouter.post("/add", async (req, res) => {
     res.status(500).json({ message: "Error adding this pizza option", error });
   }
 });
-optionRouter.get("/", async (req, res) => {
-    try {
-      const pizzaOptions = await PizzaOptions.find();
-      
-      if (!pizzaOptions || pizzaOptions.length === 0) {
-        return res.status(300).json({ msg: "No data added yet" });
-      }
-  
-      // Extract unique categories
-      const categories = [...new Set(pizzaOptions.map((opt) => opt.category))];
-  
-      // Categorize options
-      const categorizedOptions = categories.map((category) => ({
-        category: category,
-        data: pizzaOptions.filter((opt) => opt.category === category),
-      }));
-  
-      return res.status(200).json({ data: categorizedOptions });
-  
-    } catch (error) {
-      return res.status(500).json({ message: "Error fetching the pizza options", error });
-    }
-  });
 
-  
 optionRouter.post("/addMultiple", async (req, res) => {
   try {
-    console.log(" ====> ", req?.body);
     const options = req?.body?.options; // Expecting an array of options
 
     if (!Array.isArray(options) || options?.length === 0) {
