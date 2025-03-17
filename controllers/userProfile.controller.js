e313e4b86706078b42bf57b685b062e97f9c240f
AkritiGhosh <akritighosh98@gmail.com>
2025-03-17 21:29:19 +0530

    controller functions defined

---
+++ controllers/userProfile.controller.js
@@ -0,0 +1,48 @@
+export const createProfile = async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getProfileData = async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const editProfile = async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteProfile = async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const addNewAddress = async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const editAddress = async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteAddress = async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
