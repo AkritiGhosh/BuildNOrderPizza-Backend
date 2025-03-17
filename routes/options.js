802d3d0270082f27d6de66da8c9054d93ade3b96
AkritiGhosh <akritighosh98@gmail.com>
2025-03-17 11:18:12 +0530

    file renamed - import updated

--- routes/options.js
+++ routes/options.js
@@ -1,5 +1,5 @@
 import express from "express";
-import * as optionController from "../controllers/options.js";
+import * as optionController from "../controllers/options.controller.js";
 
 const optionRouter = express.Router();
 
