b243d2d6792c7160f827f0863b02eb3d2c15f77f
AkritiGhosh <akritighosh98@gmail.com>
2025-03-17 21:31:48 +0530

    profile routes defined

---
+++ routes/userProfile.routes.js
@@ -0,0 +1,38 @@
+import express from "express";
+import { authenticatedUse } from "../middleware/auth.middleware";
+import * as profileController from "../controllers/userProfile.controller.js";
+
+const profileRouter = express.Router();
+
+// Create new user
+profileRouter.post("/new", authenticatedUse, profileController.createProfile);
+
+// Get user information
+profileRouter.get("/:id", authenticatedUse, profileController.getProfileData);
+
+// Update user information
+profileRouter.put("/:id", authenticatedUse, profileController.editProfile);
+
+// Delete user
+profileRouter.delete("/:id", authenticatedUse, profileController.deleteProfile);
+
+// Add new address
+profileRouter.put(
+  "/:id/address/new",
+  authenticatedUse,
+  profileController.addNewAddress
+);
+
+// Edit existing address
+profileRouter.put(
+  "/:id/address/:id",
+  authenticatedUse,
+  profileController.editAddress
+);
+
+// Delete address
+profileRouter.delete(
+  "/:id/address/:id",
+  authenticatedUse,
+  profileController.deleteAddress
+);
