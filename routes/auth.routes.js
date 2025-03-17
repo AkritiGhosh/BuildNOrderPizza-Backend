79a1244ad869f8981d910520d8b46ec3be88ed03
AkritiGhosh <akritighosh98@gmail.com>
2025-03-17 21:32:05 +0530

    functions added to routes

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,18 +1,18 @@
 import express from "express";
+import * as authController from "../controllers/auth.controller.js";
 
 const authRouter = express.Router();
 
 // Register as new user using email and password
-authRouter.post('/register')
+authRouter.post("/register", authController.registerNewUser);
 
 // Login existing user using email and password
-authRouter.post('/login')
+authRouter.post("/login", authController.loginUser);
 
 // Change password
 
 // Forget password
 
 // Register/Login using Google SSO
-
 
 export default authRouter;
